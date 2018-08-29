73da3fdd9bb40a951487618443e12ef13a8003ad
jessekrubin <jessekrubin@gmail.com>
2018-08-29 11:46:34 -0700

    020

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lager',
-    version='0.1.0',
+    version='0.2.0',
     description='lager ~ a sweet way to log',
     long_description='coming soon',
     classifiers=[
