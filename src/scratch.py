14cdddd802f4734cee1cc436b3e0912000ab678c
jessekrubin <jessekrubin@gmail.com>
2018-12-06 12:36:20 -0800

    herm

--- src/scratch.py
+++ src/scratch.py
@@ -2,21 +2,15 @@
 from lager import logger
 import inspect
 
-members = inspect.getmembers(logger)
+logger.info('herm')
 
-logger.info("a message")
-logger + "herm"
-logger += "asdf"
-print(logger)
-
-logger + "some string"
-print(logger)
-def thing():
-    logger + "ohno"
-    print(logger)
-    logger + "ohno"
-    print(logger)
-    # logger += "another"
-
-
-thing()
+# members = inspect.getmembers(logger)
+#
+# logger.info("a message")
+# logger + "herm"
+# logger + "asdf"
+# def thing():
+#     logger + "another"
+#
+#
+# thing()
