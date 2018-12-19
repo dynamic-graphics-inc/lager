5ddfa1b4917123b1797a42f6b1d5910e5d32e413
jesse <jesse@dgi.com>
2018-12-19 15:26:49 -0700

    herm

---
+++ scratch.py
@@ -0,0 +1,16 @@
+# import lager
+from lager import logger
+import inspect
+
+logger.info('herm')
+
+# members = inspect.getmembers(logger)
+#
+# logger.info("a message")
+# logger + "herm"
+# logger + "asdf"
+# def thing():
+#     logger + "another"
+#
+#
+# thing()
