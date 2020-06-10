a469240ba4d4778ecba7a3b64ef071e95db1c15f
jesse <jessekrubin@gmail.com>
2020-06-10 13:21:18 -0700

    lager updates

--- afile.py
+++ afile.py
@@ -1,34 +1,36 @@
-from lager import pour_lager
+from lager import lager
 
-log = pour_lager(tornado=True)
+log = lager
 log.info('something else')
 log.debug('something else')
 # log.info('some other thing herm', howdy="something")
 log.warning('some warning')
 # log = pour_lager(filepath='herm.log')
 log.info('howdy')
-log + "herm"
-
-log += "herm plus eq"
-log + {"hermo": "herm", "somethign": "hwer"}
-print(log)
-
-
-log - "debug msg"
-log -= "debug msg"
-log * "warning msg"
-log *= "warning msg"
-log ** "error"
-log **= "error msg"
-
-log.e("error msg again")
-log.c("critical msg again")
-# for i in range(10000):
-#     log.info("info: " + str(i))
-#     log.warning("warning: " + str(i))
-#     if i % 7 == 0:
-#         log.error('DA_error: ' + str(i))
-#     if i % 13 == 0:
-#         log.debug('DDDBUG: ' + str(i))
-
-# a = load_log('herm.log')
+# log + "herm"
+#
+# log += "herm plus eq"
+# log + {
+#     "hermo": "herm",
+#     "somethign": "hwer"
+#     }
+# print(log)
+#
+# log - "debug msg"
+# log -= "debug msg"
+# log * "warning msg"
+# log *= "warning msg"
+# log ** "error"
+# log **= "error msg"
+#
+# log.e("error msg again")
+# log.c("critical msg again")
+# # for i in range(10000):
+# #     log.info("info: " + str(i))
+# #     log.warning("warning: " + str(i))
+# #     if i % 7 == 0:
+# #         log.error('DA_error: ' + str(i))
+# #     if i % 13 == 0:
+# #         log.debug('DDDBUG: ' + str(i))
+#
+# # a = load_log('herm.log')
--- lager/__init__.py
+++ lager/__init__.py
@@ -4,11 +4,13 @@ import asyncio
 import sys
 from functools import wraps
 from time import time
+from typing import TYPE_CHECKING
 from typing import Union
 
 from loguru import logger as lager
-from loguru._logger import Logger
 
+if TYPE_CHECKING:
+    from loguru._logger import Logger
 
 VERSION_MAJOR = 0
 VERSION_MINOR = 2
@@ -16,17 +18,28 @@ VERSION_PATCH = 3
 VERSION_INFO = (VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 __version__ = f"{VERSION_MAJOR}.{VERSION_MINOR}.{VERSION_PATCH}"
 
-TORNADO_LOGURU_FMT = "<level>[{level.name[0]} {time:YYMMDDTHH:mm:ss} {name}:{module}:{line}]</level> {message}"
+TORNADO_LOGURU_FMT = "".join(
+    [
+        "<level>",
+        "[{level.name[0]} ",
+        "{time:YYMMDDTHH:mm:ss} ",
+        "{name}:{module}:{line}]",
+        "</level> ",
+        "{message}",
+    ]
+)
 
-LOGURU_DEFAULT_FMT = ''.join([
-    "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>",
-    " | ",
-    "<level>{level: <8}</level>",
-    " | ",
-    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>",
-    " - ",
-    "<level>{message}</level>"
-    ])
+LOGURU_DEFAULT_FMT = "".join(
+    [
+        "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green>",
+        " | ",
+        "<level>{level: <8}</level>",
+        " | ",
+        "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>",
+        " - ",
+        "<level>{message}</level>",
+    ]
+)
 lager.t = lager.trace
 lager.d = lager.debug
 lager.i = lager.info
@@ -35,47 +48,53 @@ lager.w = lager.warning
 lager.e = lager.error
 lager.c = lager.critical
 
+# commonly used dgpy aliases
+LOG = lager
+# ln => natural log
+ln = lager
+LN = lager
+
+LOG_LEVELS = {
+    "notset": "NOTSET",
+    "n": "NOTSET",
+    "debug": "DEBUG",
+    "d": "DEBUG",
+    "info": "INFO",
+    "i": "INFO",
+    "s": "SUCCESS",
+    "success": "SUCCESS",
+    "warning": "WARNING",
+    "warn": "WARNING",
+    "w": "WARNING",
+    "error": "ERROR",
+    "e": "ERROR",
+    "critical": "CRITICAL",
+    "fatal": "CRITICAL",
+    "c": "CRITICAL",
+    # enum/enum-strings
+    "0": "NOTSET",
+    "10": "DEBUG",
+    "20": "INFO",
+    "25": "SUCCESS",
+    "30": "WARNING",
+    "40": "ERROR",
+    "50": "CRITICAL",
+}
+
 
 def loglevel(level: Union[str, int]) -> str:
-    log_levels = {
-        "notset"  : "NOTSET",
-        "n"       : "NOTSET",
-        "debug"   : "DEBUG",
-        "d"       : "DEBUG",
-        "info"    : "INFO",
-        "i"       : "INFO",
-        "s"       : "SUCCESS",
-        "success" : "SUCCESS",
-        "warning" : "WARNING",
-        "warn"    : "WARNING",
-        "w"       : "WARNING",
-        "error"   : "ERROR",
-        "e"       : "ERROR",
-        "critical": "CRITICAL",
-        "fatal"   : "CRITICAL",
-        "c"       : "CRITICAL",
-        # enum/enum-strings
-        "0"       : "NOTSET",
-        "10"      : "DEBUG",
-        "20"      : "INFO",
-        "25"      : "SUCCESS",
-        "30"      : "WARNING",
-        "40"      : "ERROR",
-        "50"      : "CRITICAL",
-        }
-    if isinstance(level, int):
-        return loglevel(str(level))
-    return log_levels[level.strip('\'').strip('\"').lower()]
+    """Convert log-level abrev to a valid loguru log level"""
+    return LOG_LEVELS[str(level).strip("'").strip('"').lower()]
 
 
-def pour_lager(level="d", filepath=None, stderr=True, ) -> Logger:
+def pour_lager(level="DEBUG", filepath=None, stderr=True) -> Logger:
     level = loglevel(level)
     if level != "DEBUG":
         lager.remove()
     if stderr:
-        lager.add(sys.stderr, level=level)
+        lager.add(sys.stderr, level=level, serialize=True)
     if filepath:
-        lager.add(filepath, level=level)
+        lager.add(filepath, level=level, serialize=True)
     return lager
 
 
@@ -107,10 +126,8 @@ def flog(funk=None, level="debug", enter=True, exit=True):
 
     """
 
-
     def _flog(funk):
         name = funk.__name__
-
 
         @wraps(funk)
         def _flog_decorator(*args, **kwargs):
@@ -122,7 +139,7 @@ def flog(funk=None, level="debug", enter=True, exit=True):
                     name,
                     args,
                     kwargs,
-                    )
+                )
             ti = time()
             result = funk(*args, **kwargs)
             tf = time()
@@ -133,9 +150,8 @@ def flog(funk=None, level="debug", enter=True, exit=True):
                     name,
                     result,
                     tf - ti,
-                    )
+                )
             return result
-
 
         @wraps(funk)
         async def _flog_decorator_async(*args, **kwargs):
@@ -147,7 +163,7 @@ def flog(funk=None, level="debug", enter=True, exit=True):
                     name,
                     args,
                     kwargs,
-                    )
+                )
             ti = time()
             result = await funk(*args, **kwargs)
             tf = time()
@@ -158,13 +174,16 @@ def flog(funk=None, level="debug", enter=True, exit=True):
                     name,
                     result,
                     tf - ti,
-                    )
+                )
             return result
-
 
         if asyncio.iscoroutinefunction(funk) or asyncio.iscoroutine(funk):
             return _flog_decorator_async
         return _flog_decorator
 
-
     return _flog(funk) if funk else _flog
+
+
+def handlers():
+    """Return all handlers"""
+    return lager._core.handlers
